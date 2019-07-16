a7d2df6a92d6e13283709e29c861c738b9b68c49
furbyus <furbyus@gmail.com>
2019-07-16 11:55:28 +0200

    add sort-packages and commented serviceProvider

--- src/LaravelServiceProvider.php
+++ src/LaravelServiceProvider.php
@@ -6,12 +6,15 @@ use Illuminate\Support\ServiceProvider;
 class LaravelServiceProvider extends ServiceProvider
 {
     /**
-     * Bootstrap any application services.
+     * Bootstrap application services.
      *
      * @return void
      */
     public function boot()
     {
+        /*
+        * Publish the urapi.php config file
+        */
         $this->publishes([
             __DIR__ . '/config/urapi.php' => config_path('urapi.php'),
         ]);
