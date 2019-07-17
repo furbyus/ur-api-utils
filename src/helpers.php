ff8e8ae1f8dc091b71874b6b8794325b8be560ed
furbyus <furbyus@gmail.com>
2019-07-17 12:09:59 +0200

    added app() helper in case there're no Laravel

--- src/helpers.php
+++ src/helpers.php
@@ -1,4 +1,5 @@
 <?php
+use Illuminate\Container\Container;
 
 if (!function_exists('register_fn')) {
     function register_fn($fn_name, $fn)
@@ -8,8 +9,8 @@ if (!function_exists('register_fn')) {
     }
 }
 /*
-*   Example decarations of test functions sum_test, the two ways...
-*/
+ *   Example decarations of test functions sum_test, the two ways...
+ */
 register_fn('sum_test', function ($v1, $v2) {
     return $v1 + $v2;
 });
@@ -21,12 +22,12 @@ if (!function_exists('sum_test')) {
 }
 
 /*
-*   Helper functions
-*/
+ *   Helper functions
+ */
 
 /*
-*   Function response (if package is installed outside Laravel)
-*/
+ *   Function response (if package is installed outside Laravel)
+ */
 register_fn('response', function ($content = '', $status = 200, $headers = [], $info = []) {
     $n = func_num_args();
 
@@ -38,34 +39,50 @@ register_fn('response', function ($content = '', $status = 200, $headers = [], $
     $factory = new UrApi\Utils\ResponseFactory;
     return $factory->make($content, $status, $headers, $info);
 });
+if (!function_exists('app')) {
+    /**
+     * Get the available container instance.
+     *
+     * @param  string|null  $abstract
+     * @param  array   $parameters
+     * @return mixed|\Illuminate\Contracts\Foundation\Application
+     */
+    function app($abstract = null, array $parameters = [])
+    {
+        if (is_null($abstract)) {
+            return Container::getInstance();
+        }
 
+        return Container::getInstance()->make($abstract, $parameters);
+    }
+}
 /*
-*   Function $o2a (Object to Array)
-*/
+ *   Function $o2a (Object to Array)
+ */
 register_fn('o2a', function ($data) {
     return (array) $data;
 });
 /*
-*   Function $a2o (Array to Object)
-*/
+ *   Function $a2o (Array to Object)
+ */
 register_fn('a2o', function ($data) {
     return (object) $data;
 });
 /*
-*   Function uresponse helper like Illuminate/Foundation/helpers.php_>response helper, because 'response' function is already declared and it cannot be re-declared
-*/
+ *   Function uresponse helper like Illuminate/Foundation/helpers.php_>response helper, because 'response' function is already declared and it cannot be re-declared
+ */
 if (!function_exists('uresponse')) {
     function uresponse($content = '', $status = 200, $headers = [], $info = [])
     {
-
+        $factory = app(UrApi\Utils\ResponseFactory::class);
+       
         $n = func_num_args();
 
         if ($n === 0) {
-            print 'n=0';
-            $factory = new UrApi\Utils\ResponseFactory;
             return $factory->get();
         }
-        $factory = new UrApi\Utils\ResponseFactory;
+
         return $factory->make($content, $status, $headers, $info);
+
     }
 }
